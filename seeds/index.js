878e584889b4f21381078b4e599ad282372b1ce8
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-04-01 18:39:46 -0400

    added basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -19,15 +19,17 @@ const sample = (array) => {
     return array[Math.floor(Math.random() * array.length)]
 }
 
-
-
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
+        const price = Math.floor(Math.random() *20) + 10;
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet tortor vitae turpis ultrices fringilla. Vestibulum ac sem ac libero lobortis ornare. Donec sit amet tristique turpis.",
+            price
         })
         await camp.save()
     }
