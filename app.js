79620a480d7c8fce8b9bcdb65409421def48b282
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-04-05 20:40:50 -0400

    added basic error handling & joi validation

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const Joi = require("joi");
+const {campgroundSchema} = require("./schemas.js");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const { join } = require("path");
 
 
 mongoose.connect("mongodb://localhost:27017/yelpcamp", {
@@ -27,45 +32,68 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true})); //allows body to be parsed so we can use req.body
 app.use(methodOverride("_method"));
 
+
+const validateCampground = (req, res, next) => {  
+    const {error} = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+
 app.get("/", (req,res) => {
     res.render("home")
 })
 
-app.get("/campgrounds", async (req,res) => { //async bc the mongoose function takes time
+app.get("/campgrounds", catchAsync(async (req,res) => { //async bc the mongoose function takes time
     const campgrounds = await Campground.find() //queries the database for all instances of the model and saves to a variable
     res.render("campgrounds/index", {campgrounds})
-})
+}))
 
 app.get("/campgrounds/new", (req, res) => { //this route has to be before the /:id route or it will treat "new" as :id
     res.render("campgrounds/new");
 })
 
-app.post("/campgrounds", async (req, res) => {
+app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get("/campgrounds/:id", async (req,res) => { //async bc the mongoose function takes time
+app.get("/campgrounds/:id", catchAsync(async (req,res) => { //async bc the mongoose function takes time
     const campground = await Campground.findById(req.params.id)
     res.render("campgrounds/show", {campground})
-})
+}))
 
-app.get("/campgrounds/:id/edit", async (req,res) => {
+app.get("/campgrounds/:id/edit", catchAsync(async (req,res) => {
     const campground = await Campground.findById(req.params.id)
     res.render("campgrounds/edit", {campground})
-})
+}))
 
-app.put("/campgrounds/:id", async (req,res) => {
+app.put("/campgrounds/:id", validateCampground, catchAsync(async (req,res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground} ) //spread operator spreads into properties into new object so we can use here as {title: "fdfs"}, location: "skjdfd"
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete("/campgrounds/:id", async (req,res) => {
+app.delete("/campgrounds/:id", catchAsync(async (req,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds")
+}))
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404))
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = "Oh no, something went wrong!"
+    res.status(statusCode).render("error", {err});
 })
 
 app.listen(3000, () => {
