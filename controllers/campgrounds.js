a09c9fc807a8ee6b864e3e40c844a30f049d2a1e
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-04-27 20:34:34 -0400

    added geocoding & map

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require("../models/campground");
 const ObjectID = require('mongodb').ObjectID;
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({accessToken: mapBoxToken});
 const {cloudinary} = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
@@ -12,7 +15,13 @@ res.render("campgrounds/new")
 }
 
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send();
+    
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
     campground.author = req.user._id;
     await campground.save();
@@ -22,12 +31,11 @@ module.exports.createCampground = async (req, res, next) => {
 }
 
 module.exports.showCampground = async (req,res, next) => { 
-    try {
-        const {id} = req.params;
-        if (!ObjectID.isValid(id)) {
-            req.flash("error", "Campground not found!")
-            return res.redirect("/campgrounds");
-        }
+    const {id} = req.params;
+    if (!ObjectID.isValid(id)) {
+         req.flash("error", "Campground not found!")
+        return res.redirect("/campgrounds");
+    }
     const campground = await Campground.findById(req.params.id).populate({
         path: "reviews",
         populate: {
@@ -40,9 +48,6 @@ module.exports.showCampground = async (req,res, next) => {
         return res.redirect("/campgrounds");
     } 
     res.render("campgrounds/show", {campground})
-    } catch (e){
-        next(e);
-    }
 }
 
 module.exports.renderEditForm = async (req,res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
-const Campground = require("../models/campground");
-const cities = require("./cities")
-const {places, descriptors} = require("./seedHelpers");
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-mongoose.connect("mongodb://localhost:27017/yelpcamp", {
+mongoose.connect('mongodb://localhost:27017/yelpcamp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-})
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -15,44 +15,45 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-const sample = (array) => {
-    return array[Math.floor(Math.random() * array.length)]
-}
-
+const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++){
-        const price = Math.floor(Math.random() *20) + 10;
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            title: `${sample(descriptors)} ${sample(places)}`,
+            
+            author: '607a5d6f4bc892709d998465',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            author: "607a5d6f4bc892709d998465",
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
-                url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136220/YelpCamp/xedjbhux7rfi1kihv03d.jpg", 
-                filename: "YelpCamp/xedjbhux7rfi1kihv03d" 
-            },
-            {
-                url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136174/YelpCamp/oultxkr0pjpidmdqsu1s.jpg", 
-                filename: "YelpCamp/oultxkr0pjpidmdqsu1s"
-            },
-            {
-                url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136149/YelpCamp/mcuiczu0lglmhzqhflbx.jpg", 
-                filename: "YelpCamp/mcuiczu0lglmhzqhflbx"
-            },
-            {
-                url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136123/YelpCamp/hnexyqufqmnpdso5v0ks.jpg", 
-                filename: "YelpCamp/hnexyqufqmnpdso5v0ks"
-            }
-            ],
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet tortor vitae turpis ultrices fringilla. Vestibulum ac sem ac libero lobortis ornare. Donec sit amet tristique turpis.",
-            price
+                    url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136220/YelpCamp/xedjbhux7rfi1kihv03d.jpg", 
+                    filename: "YelpCamp/xedjbhux7rfi1kihv03d" 
+                },
+                {
+                    url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136174/YelpCamp/oultxkr0pjpidmdqsu1s.jpg", 
+                    filename: "YelpCamp/oultxkr0pjpidmdqsu1s"
+                },
+                {
+                    url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136149/YelpCamp/mcuiczu0lglmhzqhflbx.jpg", 
+                    filename: "YelpCamp/mcuiczu0lglmhzqhflbx"
+                },
+                {
+                    url: "https://res.cloudinary.com/dw3o86f8j/image/upload/v1619136123/YelpCamp/hnexyqufqmnpdso5v0ks.jpg", 
+                    filename: "YelpCamp/hnexyqufqmnpdso5v0ks"
+                }
+            ]
         })
-        await camp.save()
+        await camp.save();
     }
 }
-
 seedDB().then(() => {
-    mongoose.connection.close()
-});
+    mongoose.connection.close();
+})
