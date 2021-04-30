398d989e31f064c424244adacacddd348278ba4f
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-04-29 21:43:37 -0400

    updated seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -26,7 +26,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             
-            author: '607a5d6f4bc892709d998465',
+            author: '608b5ed84e7d86bf77f8a601',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
